ac22f6856c20223220096340994c2672d0fb86d7
alelajoseph <alelajoseph@gmail.com>
2023-09-21 16:19:56 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _strcmp - compare 2 strings.
- * @s1: string to be compared.
- * @s2: string for comparation.
- * Return: Difference.
+ * _strcmp - compare two strings
+ * @s1: string to compare to s2
+ * @s2: string to concate to s1
+ * Return: Same 0, Different s1 - s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	while (*s1 && *s2)
 	{
+		if (*s1 - *s2 != 0)
+		{
+			return (*s1 - *s2);
+		}
 		s1++;
 		s2++;
 	}
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else
-	{
-	return (*s1 - *s2);
-	}
+	return (0);
 }
