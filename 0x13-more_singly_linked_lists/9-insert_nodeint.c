56f3ec3906b3b9ecadc8e629eaa36fb579726186
alelajoseph <alelajoseph@gmail.com>
2023-10-24 16:38:53 -0700

    correct

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,64 +1,39 @@
 #include "lists.h"
-
 /**
- *  * insert_nodeint_at_index - returns the nth node of a linked list
- *   * @head: pointer to the head of the list
- *    * @idx: index of the node to be added
- *     * @n: content of the new node
- *      *
- *       * Return: the address of the node
- */
+ *  * insert_nodeint_at_index - inserts a new node at given position
+ *   * @head: head of linked list
+ *    * @idx: index of new node
+ *     * @n: number for n element of struct of linked list
+ *      * Return: address of new node
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node = NULL;
-listint_t *previous_node = NULL;
+listint_t *new_node, *temp_node = *head;
 unsigned int i = 0;
 
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL || idx > listint_len(*head))
+while (temp_node && i < idx - 1)
 {
-free(new_node);
-return (NULL);
+temp_node = temp_node->next;
+i++;
 }
+if (temp_node || (i == idx - 1 || idx == 0))
+{
+new_node = malloc(sizeof(listint_t));
+if (new_node)
+{
 new_node->n = n;
-new_node->next = NULL;
-while (head != NULL)
-{
-if (i == idx)
-{
-if (i == 0)
+if (idx == 0)
 {
 new_node->next = *head;
 *head = new_node;
+}
+else
+{
+new_node->next = temp_node->next;
+temp_node->next = new_node;
+}
 return (new_node);
 }
-																								new_node->next = previous_node->next;
-previous_node->next = new_node;
-return (new_node);
-}
-else if ((i + 1) == idx)
-previous_node = *head;
-head = &((*head)->next);
-i++;
 }
 return (NULL);
-}
-
-/**
- *  * listint_len - counts the number of nodes in a linked list
- *   * @h: head of the list
- *    *
- *     * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-const listint_t *cursor = h;
-size_t count = 0;
-
-while (cursor != NULL)
-{
-count += 1
-cursor = cursor->next;
-}
-return (count);
 }
