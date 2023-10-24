cadd32ee180adcec4e493262342ec40bc7aaa597
alelajoseph <alelajoseph@gmail.com>
2023-10-23 17:01:36 -0700

    Task 1 commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,10 +11,10 @@ size_t listint_len(const listint_t *h)
 const listint_t *cursor = h;
 size_t count = 0;
 
-	while (cursor != NULL)
-	{
-	count += 1;
-	cursor = cursor->next;
-	}
+while (cursor != NULL)
+{
+count += 1;
+cursor = cursor->next;
+}
 return (count);
 }
