e2736e5ee77bbd9003191e890fa8d6fc767b0b79
alelajoseph <alelajoseph@gmail.com>
2023-10-24 21:56:31 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,15 +5,15 @@
  *  * list_len - lenght of the list
  *   * @h: points to node
  *    * Return: number of nodes
- *     */
+*/
 size_t list_len(const list_t *h)
 {
-		unsigned int i = 0;
+unsigned int i = 0;
 
-			while (h != NULL)
-					{
-								i++;
-										h = h->next;
-											}
-				return (i);
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
 }
