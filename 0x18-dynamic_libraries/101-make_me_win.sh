877f2a7daeef02f9172bd2cca1d9e6538cf517fb
alelajoseph <alelajoseph@gmail.com>
2023-12-17 02:33:10 -0800

    updated hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/alelajoseph/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/alelajoseph/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD="$PWD/../win.so"
